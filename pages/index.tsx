d08118411b739ba5bb691ef0345cfd6b2459f5a0
levg34 <levg34@users.noreply.github.com>
2022-03-22 22:23:20 +0100

    get server status

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,53 +1,56 @@
 import type { NextPage } from 'next'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Badge, Button, ButtonGroup, Card, Container } from 'react-bootstrap'
 import useSWR, { useSWRConfig } from 'swr'
 
 const fetcher = (...args: [RequestInfo, RequestInit | undefined]) => fetch(...args).then(res => res.json())
 
-type ServerState = 'started' | 'stopped' | 'error'
+type ServerState = 'started' | 'stopped' | 'starting...' | 'stopping...'
 
 const Home: NextPage = () => {
-  const [serverState, setServerState] = useState<ServerState>('stopped')
-  const {data,error} = useSWR('/api/logs',fetcher)
-
   const { mutate } = useSWRConfig()
 
-  const logs = data?.stdout
+  const [serverState, setServerState] = useState<ServerState>('stopped')
+  const {data: logsResponse} = useSWR('/api/logs',fetcher)
+  const {data: statusResponse} = useSWR('/api/server-status',fetcher)
+
+  const serverPID = statusResponse?.stdout
+
+  useEffect(() => {
+    setServerState(serverPID ? 'started' : 'stopped')
+  },[statusResponse])
+
+  const logs = logsResponse?.stdout
 
   const startServer = async () => {
+    setServerState('starting...')
     const res = await fetcher('/api/start-server',undefined)
-    mutate('/api/logs')
+    refreshData()
     console.log(res)
-    setServerState('started')
   }
 
   const stopServer = async () => {
+    setServerState('stopping...')
     const res = await fetcher('/api/stop-server',undefined)
-    mutate('/api/logs')
+    refreshData()
     console.log(res)
-    setServerState('stopped')
   }
-
-  // const restartServer = () => {
-  //   setServerState('error')
-  // }
 
   const refreshData = () => {
     mutate('/api/logs')
+    mutate('/api/server-status')
   }
 
   return (
     <Container>
       <br/>
       <Card body>
-        BeamMP server status: <Badge bg={serverState === 'started' ? 'success' : 'danger'}>{serverState}</Badge>
+        BeamMP server status: <Badge bg={serverState === 'started' ? 'success' : (serverState === 'stopped' ? 'danger' : 'warning')}>{serverState}</Badge>
       </Card>
       <br/>
       <ButtonGroup>
-        <Button variant="success" disabled={serverState === 'started'} onClick={startServer}>Start</Button>
-        <Button variant="danger" disabled={serverState === 'stopped'} onClick={stopServer}>Stop</Button>
-        {/* <Button variant="warning" disabled={serverState === 'stopped'} onClick={restartServer}>Restart</Button> */}
+        <Button variant="success" disabled={serverState === 'started' || serverState === 'starting...'} onClick={startServer}>Start</Button>
+        <Button variant="danger" onClick={stopServer}>Stop</Button>
       </ButtonGroup>{' '}
       <Button variant="primary" onClick={refreshData}>Refresh</Button>
       <br/>
