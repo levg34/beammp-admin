a3cecf8f1e5f80937cfa38d301ee5f6bea250fb0
levg34 <levg34@users.noreply.github.com>
2022-04-06 19:21:11 +0200

    reset SSH connexion

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,7 +27,8 @@ const Home: NextPage = () => {
     setServerState('starting...')
     const res = await fetcher('/api/start-server',undefined)
     refreshData()
-    setTimeout(refreshData,2500)
+    setTimeout(refreshData,1500)
+    setTimeout(refreshData,3500)
     console.log(res)
   }
 
@@ -36,12 +37,19 @@ const Home: NextPage = () => {
     const res = await fetcher('/api/stop-server',undefined)
     refreshData()
     setTimeout(refreshData,5000)
+    setTimeout(refreshData,10000)
     console.log(res)
   }
 
   const refreshData = () => {
     mutate('/api/logs')
     mutate('/api/server-status')
+  }
+
+  const resetSSH = async () => {
+    const res = await fetcher('/api/reset-ssh',undefined)
+    refreshData()
+    console.log(res.stdout === 'ok')
   }
 
   return (
@@ -55,7 +63,8 @@ const Home: NextPage = () => {
         <Button variant="success" disabled={serverState === 'started' || serverState === 'starting...'} onClick={startServer}>Start</Button>
         <Button variant="danger" onClick={stopServer}>Stop</Button>
       </ButtonGroup>{' '}
-      <Button variant="primary" onClick={refreshData}>Refresh</Button>
+      <Button variant="primary" onClick={refreshData}>Refresh</Button>{' '}
+      <Button variant="warning" onClick={resetSSH}>Reset SSH connexion</Button>
       <br/>
       <br/>
       <Card body as="pre">{logs}</Card>
