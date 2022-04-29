20d9e039d2ae427ac40c168d2f01882895c79581
levg34 <levg34@users.noreply.github.com>
2022-04-29 13:10:05 +0200

    insert users on start (after log rotate)

--- pages/api/import-users.ts
+++ pages/api/import-users.ts
@@ -1,13 +1,22 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { SSHExecCommandResponse } from 'node-ssh'
-import extractUsersToDb from '../../utils/extractUsersToDb'
+import extractUsersToDb, { ExtractReport } from '../../utils/extractUsersToDb'
+
+type ErrorReturnType = {
+  error: any
+}
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse
+  res: NextApiResponse<ExtractReport | ErrorReturnType>
 ) {
-  const inserted = await extractUsersToDb()
+
+  let inserted = {}
+  try {
+    inserted = await extractUsersToDb()
+  } catch (error) {
+    res.status(500).json({error})
+  }
 
   res.status(200).json(inserted)
 }
