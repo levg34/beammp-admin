95b3024c2052b9fbe5a52cf12a804e1b0f0e1c1c
levg34 <guilhem.dross@laposte.net>
2022-05-24 20:04:06 +0200

    config form editable

--- classes/ServerConfig.ts
+++ classes/ServerConfig.ts
@@ -1,8 +1,11 @@
 import toml from 'toml'
 import _ from 'lodash'
+import json2toml from 'json2toml'
+
+type ServerConfigValue = string | number | boolean
 
 export type ServerConfigType = {
-    General: Record<string,string | number | boolean>
+    General: Record<string,ServerConfigValue>
 }
 
 export default class ServerConfig {
@@ -30,5 +33,29 @@ export default class ServerConfig {
 
     configObject(): ServerConfigType['General'] {
         return this.config.General
+    }
+
+    toTOML(): string {
+        return json2toml(this.config, { indent: 0, newlineAfterSection: true })
+    }
+
+    editConfig(key: string, value: ServerConfigValue): ServerConfig {
+        const editedConfig = new ServerConfig()
+        editedConfig.config = {...this.config}
+        const confVal = editedConfig.config.General[key]
+
+        if (typeof confVal === 'string') {
+            editedConfig.config.General[key] = value
+        } else if (typeof confVal === 'number') {
+            const numberValue = Number(value)
+            if (value !== '' && Number.isInteger(numberValue)) {
+                editedConfig.config.General[key] = numberValue
+            }
+        } else if (typeof confVal === 'boolean') {
+            editedConfig.config.General[key] = value
+        } else {
+            editedConfig.config.General[key] = value
+        }
+        return editedConfig
     }
 }
--- pages/config.tsx
+++ pages/config.tsx
@@ -1,8 +1,10 @@
 import { NextPage } from 'next'
 import Link from 'next/link'
 import { SSHExecCommandResponse } from 'node-ssh'
-import { Button, Card, Container, Form, FormControl, InputGroup } from 'react-bootstrap'
+import { Alert, Button, Card, Container, Form, FormControl, InputGroup } from 'react-bootstrap'
 import useSWR from 'swr'
+import _ from 'lodash'
+import { IconAlertTriangle } from '@tabler/icons'
 import { fetcher } from '../utils/swrUtils'
 import { useEffect, useState } from 'react'
 import ServerConfig, { ServerConfigType } from '../classes/ServerConfig'
@@ -10,13 +12,16 @@ import ServerConfig, { ServerConfigType } from '../classes/ServerConfig'
 const ConfigPage: NextPage = () => {
     const {data: configResponse} = useSWR<SSHExecCommandResponse>('/api/config',fetcher)
     const configString = configResponse?.stdout
-    const [config, setParsedConfig] = useState<ServerConfig>(new ServerConfig())
+    const [config, setConfig] = useState<ServerConfig>(new ServerConfig())
+    const [warning, setWarning] = useState<boolean>()
 
     useEffect(() => {
-        if (configString) {
-            setParsedConfig(new ServerConfig(configString))
+        if (configString && _.isEmpty(config.configObject())) {
+            setConfig(new ServerConfig(configString))
+        } else if (configString) {
+            setWarning(true)
         }
-    },[configString])
+    }, [configString])
 
     const saveConfig = async () => {
         const response = await fetcher('/api/save-config', {
@@ -30,22 +35,44 @@ const ConfigPage: NextPage = () => {
         console.log(response)
     }
 
+    const toTOML = () => {
+        alert(config.toTOML())
+    }
+
+    const reloadConfig = () => {
+        setConfig(new ServerConfig(configString))
+        setWarning(false)
+    }
+
+    const handleValueChange = (event: { target: { value: string } }, key: string) => {
+        const { value } = event.target
+        config.config
+        setConfig(config.editConfig(key, value))
+    }
+
+    const handleCheckboxChange = (event: { target: { checked: boolean } }, key: string) => {
+        const { checked } = event.target
+        setConfig(config.editConfig(key, checked))
+    }
+
     return <Container>
         <Link href={'/'} passHref><Button>Return to server monitoring page</Button></Link>
         <br/><br/>
+        {warning && <Alert variant='warning'><IconAlertTriangle/> Config has been modified on the server. <Alert.Link onClick={reloadConfig}>Reload</Alert.Link> config from server?</Alert>}
         <Form as={Card} body>
             {Object.keys(config.configObject()).map(confItem => {
                 const configValue = config.configObject()[confItem];
                 return <InputGroup key={confItem} className="mt-2">
                     <InputGroup.Text>{confItem}</InputGroup.Text>
-                    {typeof configValue === 'string' && <FormControl type="text" value={configValue}/>}
-                    {typeof configValue === 'number' && <FormControl type="number" value={configValue}/>}
-                    {typeof configValue === 'boolean' && <div className="d-flex align-items-center"><Form.Check className="ms-3" type="switch" checked={configValue}/></div>}
+                    {typeof configValue === 'string' && <FormControl type="text" value={configValue} onChange={e => handleValueChange(e,confItem)}/>}
+                    {typeof configValue === 'number' && <FormControl type="number" value={configValue} onChange={e =>handleValueChange(e,confItem)}/>}
+                    {typeof configValue === 'boolean' && <div className="d-flex align-items-center"><Form.Check className="ms-3" type="switch" checked={configValue} onChange={e => handleCheckboxChange(e,confItem)}/></div>}
                 </InputGroup>
             })}
             <InputGroup className="mt-2">
                 <Button variant="success" onClick={saveConfig}>Save config</Button>{' '}
                 <Button variant="warning" disabled>Restore config</Button>
+                <Button variant="secondary" onClick={toTOML}>Save to TOML</Button>
             </InputGroup>
         </Form>
         <br/>
